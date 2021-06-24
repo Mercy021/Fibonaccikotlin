3e546a162096dbbac442ea2582da9c3d9317ccc0
mercy021 <kinyanjuimercywangari@gmail.com>
2021-06-24 13:49:58 +0300

    first commit

---
+++ app/src/main/java/com/example/recylerviewfibonacci/NumbersrvAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.recylerviewfibonacci
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.math.BigInteger
+
+class NumbersrvAdapter(var numbersList:MutableList<BigInteger>): RecyclerView.Adapter<NumbersViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumbersViewHolder {
+        val itemView=
+            LayoutInflater.from(parent.context).inflate(R.layout.numbers_items,parent,false)
+        return NumbersViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: NumbersViewHolder, position: Int) {
+        holder.tvNumber.text=numbersList.get(position).toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return numbersList.size
+    }
+}
+class NumbersViewHolder(val itemView: View): RecyclerView.ViewHolder(itemView){
+    val tvNumber=itemView.findViewById<TextView>(R.id.tvNumbers)
+
+}
